edeb7cab9284d1bc8b031ceb4b7258b1371412ad
Denis <denis09112004@gmail.com>
2024-06-11 18:00:17 +0300

    database exception created

---
+++ TaskMetrics.Infrastructure/Exceptions/DatabaseException.cs
@@ -0,0 +1,8 @@
+﻿namespace task_api.TaskMetrics.Infrastructure.Exceptions;
+
+[Serializable]
+public class DatabaseException: Exception
+{
+    public DatabaseException(string message): base(message) { }
+    public DatabaseException() : base(String.Format("Database not found")){ }
+}
