6ef9e5d11072d88b96432713fb5be612b6553ede
Denis <denis09112004@gmail.com>
2024-06-11 17:59:20 +0300

    base entity class

---
+++ TaskMetrics.Domain/Base/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace task_api.TaskMetrics.Domain.Base;
+
+public abstract class BaseEntity
+{
+    
+}
+
+public abstract class BaseEntity<TKey> : BaseEntity
+{
+    public TKey Id { get; set; }
+}
