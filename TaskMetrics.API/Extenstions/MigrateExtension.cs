0772734f67c367506268141c0064c013bc694614
Denis <denis09112004@gmail.com>
2024-06-11 22:02:50 +0300

    migration extension

---
+++ TaskMetrics.API/Extenstions/MigrateExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using task_api.TaskMetrics.Infrastructure;
+
+namespace task_api.TaskMetrics.API.Extenstions;
+
+public static class MigrateExtension
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
+        {
+            using ApplicationDbContext context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            {
+                context.Database.Migrate();
+            }
+        }
+    }
+}
