291e55949699be01c0536f1dfb792a03cb88ca44
Denis <denis09112004@gmail.com>
2024-06-11 22:03:50 +0300

    custom exception handler (return status code)

---
+++ TaskMetrics.API/Handlers/GlobalExceptionFilter.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using task_api.TaskMetrics.API.Exceptions;
+
+namespace task_api.TaskMetrics.API.Handlers;
+
+public class GlobalExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        var statusCode = context.Exception switch
+        {
+            EntityNotFoundException => StatusCodes.Status404NotFound,
+            
+            ValidationException => StatusCodes.Status400BadRequest,
+
+            UnauthorizedAccessException => StatusCodes.Status401Unauthorized,
+
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        context.Result = new ObjectResult(new
+        {
+            error = context.Exception.Message,
+            stackTrace = context.Exception.StackTrace
+        })
+        {
+            StatusCode = statusCode
+        };
+    }
+}
