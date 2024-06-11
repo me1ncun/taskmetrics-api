7cdc38eb15a653782b1a8382e5414cf6f31ef5e2
Denis <denis09112004@gmail.com>
2024-06-11 09:58:50 +0300

    .

---
+++ TaskMetrics.Domain/TaskRecord.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace task_api.Domain;
+
+public class TaskRecord
+{
+    public int Id { get; set; }
+    public DateTime DateCompleted { get; set; }
+    public int TimeSpent { get; set; }
+    [ForeignKey("TaskItem")]
+    public int TaskItemId { get; set; }
+    public TaskItem TaskItem { get; set; }
+    [ForeignKey("User")]
+    public int UserId { get; set; }
+    public User User { get; set; }
+}
