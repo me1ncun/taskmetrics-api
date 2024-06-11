fb2bf89139ba55fd92e42204697062bcbfecfe50
Denis <denis09112004@gmail.com>
2024-06-11 10:05:04 +0300

    created database

---
+++ TaskMetrics.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using task_api.Domain;
+
+namespace task_api.TaskMetrics.Infrastructure;
+
+public class ApplicationDbContext: DbContext
+{
+    private IConfiguration Configuration;
+
+    public ApplicationDbContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(Configuration.GetConnectionString("Database"));
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<TaskItem> TaskItems { get; set; }
+    public DbSet<TaskRecord> TaskRecords { get; set; }
+}
