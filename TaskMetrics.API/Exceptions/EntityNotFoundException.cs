945f7a0007856e192959f49da2ad7e47494aeefa
Denis <denis09112004@gmail.com>
2024-06-11 18:38:23 +0300

    created custom exception

---
+++ TaskMetrics.API/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace task_api.TaskMetrics.API.Exceptions;
+
+[Serializable]
+public class EntityNotFoundException: Exception
+{
+    public EntityNotFoundException(string message): base(message){ }
+    
+    public EntityNotFoundException() : base(String.Format("Entity not found")){ }
+}
