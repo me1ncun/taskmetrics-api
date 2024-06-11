ae352a31c4f32b0240c6aa91ae9b4a5f77fd7c1f
Denis <denis09112004@gmail.com>
2024-06-11 22:03:29 +0300

    created password hasher by sha256

---
+++ TaskMetrics.API/Helpers/HashPasswordHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace task_api.TaskMetrics.API.Helpers;
+
+public class HashPasswordHelper
+{
+    public string HashPassword(string password)
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            var hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+            var hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+
+            return hash;
+        }
+    }
+
+    public bool VerifyPassword(string password, string userPassword)
+    {
+        var hashedPassword = HashPassword(password);
+    
+        return hashedPassword == userPassword;
+    }
+}
