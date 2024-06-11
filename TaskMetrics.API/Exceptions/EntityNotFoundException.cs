da9601833ad5c5aea80e51ae55a54ea23603f338
Denis <denis09112004@gmail.com>
2024-06-11 22:44:33 +0300

    deleted not correct exceptions

--- TaskMetrics.API/Exceptions/EntityNotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace task_api.TaskMetrics.API.Exceptions;
-
-[Serializable]
-public class EntityNotFoundException: Exception
-{
-    public EntityNotFoundException(string message): base(message){ }
-    
-    public EntityNotFoundException() : base(String.Format("Entity not found")){ }
-}
--- TaskMetrics.Infrastructure/Exceptions/DatabaseException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace task_api.TaskMetrics.Infrastructure.Exceptions;
-
-[Serializable]
-public class DatabaseException: Exception
-{
-    public DatabaseException(string message): base(message) { }
-    public DatabaseException() : base(String.Format("Database not found")){ }
-}
