94c1ef1ed90cd70d7920e5ac231fee47e2f24333
Denis <denis09112004@gmail.com>
2024-06-12 13:36:24 +0300

    database creation factory

---
+++ TaskMetrics.Infrastructure/ApplicationDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using task_api.TaskMetrics.Infrastructure;
+
+namespace task_api.TaskMetrics.Domain;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("TaskMetrics.API/appsettings.json")
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("Database");
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
