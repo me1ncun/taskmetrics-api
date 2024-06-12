b96a239b7877bfd2d61fbb022d1a929f8417b1a4
Denis <denis09112004@gmail.com>
2024-06-12 13:38:39 +0300

    configured json file

--- TaskMetrics.API/Program.cs
+++ TaskMetrics.API/Program.cs
@@ -9,6 +9,10 @@ public class Program
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                config.AddJsonFile("TaskMetrics.API/appsettings.json", optional: false, reloadOnChange: true);
+            })
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
