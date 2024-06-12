9f077a89a460ed087e72340e79f8608031f961b5
Denis <denis09112004@gmail.com>
2024-06-12 15:01:07 +0300

    .

--- TaskMetrics.API/DTOs/Users/UpdateUser/UpdateUserResponse.cs
+++ TaskMetrics.API/DTOs/Users/UpdateUser/UpdateUserResponse.cs
@@ -2,6 +2,12 @@
 
 public class UpdateUserResponse
 {
+    public UpdateUserResponse(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+    
     public int Id { get; set; }
     public string Name { get; set; }
 }
