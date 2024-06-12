a30dcfa60a31f9a00f425aaa6f03510e23d17b5e
Denis <denis09112004@gmail.com>
2024-06-12 15:01:22 +0300

    created method (crud)

--- TaskMetrics.API/Controllers/UserController.cs
+++ TaskMetrics.API/Controllers/UserController.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using task_api.Domain;
+using task_api.TaskMetrics.API.DTOs.Users;
 using task_api.TaskMetrics.API.DTOs.Users.AddUser;
+using task_api.TaskMetrics.API.DTOs.Users.DeleteUser;
 using task_api.TaskMetrics.API.DTOs.Users.GetUserList;
-using task_api.TaskMetrics.API.Services.User;
+using task_api.TaskMetrics.API.DTOs.Users.UpdateUser;
 using task_api.TaskMetrics.Domain.Interfaces;
 
 namespace task_api.Controllers
@@ -19,171 +22,91 @@ namespace task_api.Controllers
         }
         
         [HttpPost("/api/user/")]
-        public async Task<IActionResult> AddUser([FromBody] AddUserRequest request)
+        public async Task<IActionResult> AddUser(AddUserRequest request)
         {
+            var userExist = await _unitOfWork.Users.GetUserByEmailAsync(request.Email);
+            if (userExist != null)
+            {
+                return BadRequest("User already exists");
+            }
+            
             var user = new User(
                 request.Name,
                 request.Email,
                 request.Password);
             
             await _unitOfWork.Users.InsertAsync(user);
+            await _unitOfWork.Save();
             
-            return Ok();
+            var response = new AddUserResponse(user.Id, user.Name);
+            
+            return Ok(response);
         }
         
-        [HttpGet("/api/user/")]
-        public async Task<IActionResult> Get()
+        [HttpGet("/api/users/")]
+        public async Task<IActionResult> GetAll()
         {
             var users = await _unitOfWork.Users.GetAllUsersAsync();
+
+            if (users.IsNullOrEmpty())
+            {
+                return NotFound();
+            }
             
             return Ok(users);
         }
         
         [HttpGet("/api/user/{id}/")]
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Details(int id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _unitOfWork.Users.GetUserByIdAsync(Convert.ToInt32(id));
+            var user = await _unitOfWork.Users.GetUserByIdAsync(id);
             if (user == null)
             {
                 return NotFound();
             }
-            return Ok(user);
+            
+            var response = new GetUserResponse(user.Id, user.Name, user.Email);
+            
+            return Ok(response);
         }
-       /* // GET: Employees/Create
-        public async Task<IActionResult> Create()
+        
+        [HttpDelete("/api/user/delete/")]
+        public async Task<IActionResult> Delete(DeleteUserRequest request)
         {
-            ViewData["DepartmentId"] = new SelectList(await _unitOfWork.Departments.GetAllAsync(), "DepartmentId", "Name");
-            return View();
-        }
-        // POST: Employees/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("EmployeeId,Name,Email,Position,DepartmentId")] Employee employee)
-        {
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    //Begin The Tranaction
-                    _unitOfWork.CreateTransaction();
-                    //Use Generic Reposiory to Insert a new employee
-                    await _unitOfWork.Employees.InsertAsync(employee);
-                    //Call SaveAsync to Insert the data into the database
-                    //await _repository.SaveAsync();
-                    //Save Changes to database
-                    await _unitOfWork.Save();
-                    //Commit the Changes to database
-                    _unitOfWork.Commit();
-                    
-                    return RedirectToAction(nameof(Index));
-                }
-                catch (Exception)
-                {
-                    //Rollback Transaction
-                    _unitOfWork.Rollback();
-                    //Log The Exception
-                }
-            }
-            ViewData["DepartmentId"] = new SelectList(await _unitOfWork.Departments.GetAllAsync(), "DepartmentId", "Name", employee.DepartmentId);
-            return View(employee);
-        }
-        // GET: Employees/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
+            var user = await _unitOfWork.Users.GetUserByIdAsync(request.Id);
+            if (user == null)
             {
                 return NotFound();
             }
-            var employee = await _unitOfWork.Employees.GetByIdAsync(id);
-            if (employee == null)
-            {
-                return NotFound();
-            }
-            ViewData["DepartmentId"] = new SelectList(await _unitOfWork.Departments.GetAllAsync(), "DepartmentId", "Name", employee.DepartmentId);
-            return View(employee);
+            
+            await _unitOfWork.Users.DeleteAsync(user.Id);
+            await _unitOfWork.Save();
+            
+            var response = new DeleteUserResponse(user.Id, user.Name);
+            
+            return Ok(response);
         }
-        // POST: Employees/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("EmployeeId,Name,Email,Position,DepartmentId")] Employee employee)
+        
+        [HttpPost("/api/user/update/")]
+        public async Task<IActionResult> Update(UpdateUserRequest request)
         {
-            if (id != employee.EmployeeId)
+            var user = await _unitOfWork.Users.GetUserByEmailAsync(request.Email);
+            if (user == null)
             {
                 return NotFound();
             }
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    //Begin The Tranaction
-                    _unitOfWork.CreateTransaction();
-                    //Use Generic Reposiory to Insert a new employee
-                    await _unitOfWork.Employees.UpdateAsync(employee);
-                    //Save Changes to database
-                    await _unitOfWork.Save();
-                    //Commit the Changes to database
-                    _unitOfWork.Commit();
-                    return RedirectToAction(nameof(Index));
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    //Rollback Transaction
-                    _unitOfWork.Rollback();
-                }
-            }
-            ViewData["DepartmentId"] = new SelectList(await _unitOfWork.Departments.GetAllAsync(), "DepartmentId", "Name", employee.DepartmentId);
-            return View(employee);
+
+            user = new User(
+                request.Name,
+                request.Email,
+                request.Password);
+            
+            await _unitOfWork.Users.UpdateAsync(user);
+            await _unitOfWork.Save();
+            
+            var response = new UpdateUserResponse(user.Id, user.Name);
+            
+            return Ok(response);
         }
-        // GET: Employees/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-            //Use Employee Repository to Fetch Employees along with the Department Data by Employee Id
-            var employee = await _unitOfWork.Employees.GetEmployeeByIdAsync(Convert.ToInt32(id));
-            if (employee == null)
-            {
-                return NotFound();
-            }
-            return View(employee);
-        }
-        // POST: Employees/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            //Begin The Tranaction
-            _unitOfWork.CreateTransaction();
-            var employee = await _unitOfWork.Employees.GetByIdAsync(id);
-            if (employee != null)
-            {
-                try
-                {
-                    await _unitOfWork.Employees.DeleteAsync(id);
-                    //Save Changes to database
-                    await _unitOfWork.Save();
-                    //Commit the Changes to database
-                    _unitOfWork.Commit();
-                }
-                catch (Exception)
-                {
-                    //Rollback Transaction
-                    _unitOfWork.Rollback();
-                }
-            }
-            return RedirectToAction(nameof(Index));
-        }
-    }*/
     }
 }
