efb3f6ac49620bd4e8ec09038da0b80545e5ba04
Denis <denis09112004@gmail.com>
2024-06-12 13:35:39 +0300

    changed congiguration

--- TaskMetrics.Infrastructure/ApplicationDbContext.cs
+++ TaskMetrics.Infrastructure/ApplicationDbContext.cs
@@ -5,18 +5,11 @@ namespace task_api.TaskMetrics.Infrastructure;
 
 public class ApplicationDbContext: DbContext
 {
-    private IConfiguration _configuration;
-
-    public ApplicationDbContext(IConfiguration configuration)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
     {
-        _configuration = configuration;
     }
     
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("Database"));
-    }
-
     public DbSet<User> Users { get; set; }
     public DbSet<TaskItem> TaskItems { get; set; }
     public DbSet<TaskRecord> TaskRecords { get; set; }
