38a05121d551b97672a2b780992d8ca3d682da32
Denis <denis09112004@gmail.com>
2024-06-13 11:51:16 +0300

    created base service

---
+++ TaskMetrics.API/Services/Base/BaseService.cs
@@ -0,0 +1,13 @@
+﻿using task_api.TaskMetrics.Domain.Interfaces;
+
+namespace task_api.TaskMetrics.API.Services;
+
+public class BaseService
+{
+    public BaseService(IUnitOfWork unitOfWork)
+    {
+        UnitOfWork = unitOfWork;
+    }
+    
+    protected IUnitOfWork UnitOfWork { get; }
+}
