5af1524c1781a2a001e0a46115524b227bbd7726
Denis <denis09112004@gmail.com>
2024-06-13 20:51:02 +0300

    registered taskservice

--- TaskMetrics.API/Extenstions/ServiceCollectionExtension.cs
+++ TaskMetrics.API/Extenstions/ServiceCollectionExtension.cs
@@ -20,7 +20,8 @@ public static class ServiceCollectionExtension
     
     public static IServiceCollection AddServices(this IServiceCollection services)
     {
-        return services.AddScoped<UserService>();
+        return services.AddScoped<UserService>()
+            .AddScoped<TaskService>();
     }
 
     public static IServiceCollection AddDatabase(this IServiceCollection services
