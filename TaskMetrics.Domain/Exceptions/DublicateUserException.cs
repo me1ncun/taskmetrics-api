95d983dd26b0e1e3de5a1df12eede31fa677fecd
Denis <denis09112004@gmail.com>
2024-06-16 16:15:11 +0300

    created exception

---
+++ TaskMetrics.Domain/Exceptions/DublicateUserException.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace task_api.TaskMetrics.Domain.Exceptions;
+
+public class DublicateUserException : Exception
+{
+    public DublicateUserException(string message) : base("User already exists"){ }
+    public DublicateUserException() : base(){ }
+}
