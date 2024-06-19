9e1c94bf6846bfa41f4262c3da2329f8688de66f
Denis <denis09112004@gmail.com>
2024-06-19 16:19:46 +0300

    refactoring

---
+++ TaskMetrics.API/DTOs/Auth/Register/RegisterUserRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace task_api.TaskMetrics.API.DTOs.Auth;
+
+public class RegisterUserRequest
+{
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Email { get; set; }
+    [Required]
+    public string Password { get; set; }
+}
---
+++ TaskMetrics.API/DTOs/Auth/Register/RegisterUserResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace task_api.TaskMetrics.API.DTOs.Auth;
+
+public class RegisterUserResponse
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string Token { get; set; }
+}
